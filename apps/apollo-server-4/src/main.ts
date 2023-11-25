72a48c6d8055aac33c5538ed86480037feaf2a09
Paul Leathwood <paul.leathwood@sky.uk>
2023-11-25 13:17:28 +0000

    schema object

--- apps/apollo-server-4/src/main.ts
+++ apps/apollo-server-4/src/main.ts
@@ -7,45 +7,8 @@ import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHt
 import { koaMiddleware } from "@as-integrations/koa";
 
 import logger from '@graphql/util-logger';
-
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-];
-
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
-
-// Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
+// import { typeDefs, resolvers } from "./resolvers";
+import { schema } from './schema';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
@@ -54,8 +17,9 @@ const app = new Koa();
 const httpServer = http.createServer(app.callback());
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  // typeDefs,
+  // resolvers,
+  schema,
   plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 await server.start();
---
+++ apps/apollo-server-4/src/schema.ts
@@ -0,0 +1,54 @@
+import { GraphQLList, GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
+
+const BASE_URL = 'https://swapi.dev/api/';
+
+function fetchResponseByURL(url) {
+  return fetch(url).then(res => res.json());
+}
+
+function fetchPeople() {
+  return fetchResponseByURL(`${BASE_URL}people`).then((json) => json.results);
+}
+
+const FilmType = new GraphQLObjectType({
+  name: 'Film',
+  fields: {
+    title: { type: GraphQLString },
+  }
+});
+
+const PersonType = new GraphQLObjectType({
+  name: 'Person',
+  fields: {
+    name: { type: GraphQLString },
+    films: {
+      type: new GraphQLList(FilmType),
+      resolve: person => person.films.map(fetchResponseByURL),
+    },
+  },
+});
+
+const QueryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: () => ({
+    allPeople: {
+      type: new GraphQLList(PersonType),
+      resolve: fetchPeople,
+    },
+    person: {
+      type: PersonType,
+      args: {
+        id: { type: GraphQLString },
+      },
+      resolve: (_root, args) => fetchResponseByURL(`${BASE_URL}people/${args.id}`),
+    },
+  }),
+});
+
+const schema = new GraphQLSchema({
+  query: QueryType,
+});
+
+export {
+  schema
+};
